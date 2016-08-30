ce15a68906008f275bc494c7fbe4a940d8f4b439
TravisAvey <avey.travis@gmail.com>
2016-08-30 13:26:38 -0500

    Got binary conversion up
    Still need to add more comments, and get Hex/Oct conversion going

--- Assignment01/main.cpp
+++ Assignment01/main.cpp
@@ -19,39 +19,189 @@ This program does
 class XmlGenerator
 {
 private:
+    // the string to hold the name of the file we are working with
     std::string fileName;
+    // the vector to hold all the input numbers
     std::vector<int> inputNumbers;
 
+    /*
+        This method initializes the file to store the
+        data in XML format
+    */
     void InitFile ()
     {
+        // open the file to write
         std::ofstream file (fileName.c_str());
+
+        // if the file is opened
         if (file)
         {
+            // output to the file the xml tag
             file << "<?xml version=\"1.1\" encoding=\"utf-8\" ?>" << std::endl;
-            file << "<assignment>";
+            // write to the file the root node
+            file << "<assignment>" << std::endl;
         }
         else
+            // we have an issue with the file
+            FileError ();
+
+        // done with the file, close it    
+        file.close ();
+    }
+
+    /*
+        This method writes the data to the file
+    */
+    void WriteFile ()
+    {
+        // open the file in append mode
+        std::ofstream file (fileName.c_str(), std::ios::app);
+
+        // assign-num counter
+        int assignNum = 1;
+        if (file)
         {
-            std::printf ("File Not Opened!\n");
+            // for each number in the input number vector
+            for (auto n : inputNumbers)
+            {
+                // ouput question element with the attribute with correct value
+                file << "\t<question assign-num=\"" << assignNum++ << "\">\n";
+
+                // output the decimal
+                file << "\t\t<decimal> " << n << " </decimal>\n";
+
+                // write to the file the binary number
+                file << "\t\t<binary> " << ConvertBinary (n) << " </binary>\n";
+
+                // write to the file the octal number
+                file << "\t\t<octal> " << ConvertOctal (n) << " </octal>\n";
+
+                // write to the file the hexadecimal number
+                file << "\t\t<hexadecimal> " << ConvertHex (n) << " </hexadecimal>\n";
+
+                // output to the file the closing quesion element
+                file << "\t</question>\n";
+            }
         }
+        else
+            // we have a issue with the file
+            FileError ();
+
+        // close the file
+        file.close ();
+    }
+
+
+    /*
+        This method converts the integer to a binary number,
+        returns the value as a string.
+
+        Format: 0b1001, removes all leading 0s
+    */
+    std::string ConvertBinary (const int &number)
+    {
+        // if the number is 0, short circuit and just return 0b0
+        if (number == 0)
+            return "0b0";
+        
+        // init a string to hold the binary value
+        std::string binary = "0b";
+        
+        // get the binary number and store as a string
+        std::string binStr = std::bitset<16> (number).to_string ();
+        
+        // declare the index i
+        int i;
+        // loop through the binary string
+        for (i = 0; i<binStr.length (); ++i)
+        {
+            // find the first 1
+            if (binStr[i] == '1')
+                break;
+        }
+        
+        // from the i position to the end, append the 1s and 0s to the binary string
+        for (int j = i; j<binStr.length (); ++j)
+            binary += binStr[j];
+        
+        // return the binary string
+        return binary;
+    }
+
+    /*
+        This method converts the number to an Octal number
+    */
+    std::string ConvertOctal (const int &number)
+    {
+        return "";
+    }
+
+    /*
+        This method converts the number to a hexadecimal number
+    */
+    std::string ConvertHex (const int &number)
+    {
+        return "";
+    }
+
+    // Helper method to output a cerr, that something went wrong with file
+    void FileError ()
+    {
+        std::cerr << "Something went wrong with the file!\n";
     }
 
 public:
-    XmlGenerator  (std::string &file)
+    /*
+        Public consructor.
+        variable file is the name of the file
+    */
+    XmlGenerator  (const std::string &file) : fileName (file)
     {
-        fileName = file;
+        // call private method to initialize the file
         InitFile ();
     }
 
+    /*
+        Pulbic method to add the number to be converted
+    */
     void AddNumber (int number)
     {
         inputNumbers.push_back (number);
     }
 
+    /*
+        This method is public interface when done inputting data
+        
+        (Possibly change this to just be the private write file method)
+    */
     void Done ()
     {
-        for (auto i : inputNumbers)
-            std::cout << i << std::endl;
+
+        WriteFile ();
+        
+        // open the file to write in append mode
+        std::ofstream file (fileName.c_str(), std::ios::app);
+
+        // if the file is opened
+        if (file)
+        {
+            // write out to the file the closing root node
+            file << "</assignment>" << std::endl;
+        }
+        else
+            FileError ();
+        
+        // close file
+        file.close ();
+    }
+
+    /*
+        This method returns the number of items that are
+        to be converted
+    */
+    int Count()
+    {
+        return inputNumbers.size ();
     }
 };
 
@@ -65,15 +215,18 @@ int main ()
 {
     std::cout << "--- Welcome to Travis Avey's Number Conversion Answer Generator ---" << std::endl;
 
-    
     std::string fileName = GetFile ();
+
     auto xmlGenerator = new XmlGenerator (fileName);
 
     GetInput (*xmlGenerator);
     
     xmlGenerator->Done ();
 
-    std::cout << "Finished. Data written to " << fileName << std::endl;
+    if (xmlGenerator->Count () > 0)
+        std::cout << "Finished. Data written to " << fileName << std::endl;
+    else
+        std::cout << "WARNING: no data to generate quiz" << std::endl;
 
     return EXIT_SUCCESS;
 }
