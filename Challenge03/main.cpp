b10e98c3da90e4ee879c77cf4b4daaabadc096d9
TravisAvey <avey.travis@gmail.com>
2016-09-21 10:18:17 -0500

    Comments done with Challenge 3
    Need to handle the &amp; in data-4, no go in XML file as is

--- Challenge03/main.cpp
+++ Challenge03/main.cpp
@@ -15,6 +15,7 @@ This program ...
 #include <vector>
 #include <map>
 #include <sstream>
+#include <locale>
 
 
 /*
@@ -29,7 +30,8 @@ private:
     std::vector<std::string> mRawData;
     // the list of countries already inputed in the XML file
     std::vector<std::string> mCountries;
-
+    // the map to store all the data for each country
+    // country name is the key, and a vector containing all the data
     std::map<std::string, std::vector<std::string> > mData;
 
     // the input file name
@@ -45,7 +47,6 @@ private:
     void GetXMLFile ();
     void WriteXML ();
     void ExtractData ();
-
     bool CountryExists (const std::string &);
 
 public:
@@ -57,10 +58,12 @@ public:
 
 };
 
+// method declaration
 bool IsGoodInputFile (std::string &);
 
 int main ()
 {
+    std::cout << "--- Welcome to Travis Avey's CSV to XML Converter ---" << std::endl;
     std::string fileName;
     if (IsGoodInputFile (fileName))
     {
@@ -71,6 +74,10 @@ int main ()
     return 0;
 }
 
+/*
+    This helper method gets the input file name from the user
+    returns true if the file is good, else returns false
+*/
 bool IsGoodInputFile (std::string &inputFile)
 {
     // prompt user for the CSV file
@@ -96,11 +103,20 @@ bool IsGoodInputFile (std::string &inputFile)
     return true;
 }
 
+/*
+    Public constructor for the ConvertXML class
+    pass in the file name to open 
+*/
 ConvertXML::ConvertXML (std::string fileName)
 {
+    // set the private file name
     mInputFile = fileName;
 }
 
+/*
+    This method calls the methods required to convert
+    the CSV to XML
+*/
 void ConvertXML::Convert ()
 {
     GetDataFromCSV ();
@@ -110,6 +126,10 @@ void ConvertXML::Convert ()
     WriteXML ();
 }
 
+/*
+    This method gets the separator from the user that
+    is used in the current CSV file
+*/
 void ConvertXML::GetSeparator ()
 {
 
@@ -144,6 +164,10 @@ void ConvertXML::GetSeparator ()
 
 }
 
+/*
+    This method gets the raw data from the csv file.
+    It places each line as a string into a vector
+*/
 void ConvertXML::GetDataFromCSV ()
 {
     // open the file using the user supplied input file name
@@ -160,85 +184,143 @@ void ConvertXML::GetDataFromCSV ()
     
 }
 
+/*
+    This method gets the XML file name from the user
+*/
 void ConvertXML::GetXMLFile ()
 {
+    // loop until we get an appropriate xml file name from the user
     while (true)
     {
+        // prompt user for the file name
         std::cout << "Enter the XML file name: ";
 
         try
         {
+            // try to get the xml file name
             std::getline (std::cin, mXmlFile);
         }
         catch (const std::exception &e)
         {
+            // if it fails, then inform the user and continue the loop
             std::cout << "\tWARNING: File name is not acceptable\n";
             continue;
         }
+        // if we get here, good file name and break
         break;
     }
 
+    // if user entered a . at the begining
     if (mXmlFile[0] == '.')
+        // create a substring removing that .
         mXmlFile = mXmlFile.substr (1);
 
+    // get the length of the xml file name
     auto len = mXmlFile.length ();
 
+    // if the lenght is less than 4, or doesn't end in .xml
     if (len <= 4 || mXmlFile.substr (len -4, len) != ".xml")
+        // add the .xml to the end of the file
         mXmlFile += ".xml";
-    
 }
 
+/*
+    This method writes all the data to the XML file
+*/
 void ConvertXML::WriteXML ()
 {
+    // open the xml file for writing
     std::fstream xmlFile (mXmlFile.c_str(), std::ios::out);
 
+    // set the locale to the machine's locale for numbers
+    xmlFile.imbue(std::locale(""));
+    // if the xml file is opened
     if (xmlFile)
     {
+        // inform user we are writing to the XML file
         std::cout << "Wrting XML file\n";
 
+        // write the xml
         xmlFile << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
+        // write the comment
         xmlFile << "<!-- Processed by Travis Avey's converter -->\n";
 
+        // write out the root tag
         xmlFile << "<countries>\n";
 
-
+        // for each country in the data map
         for (const auto &data : mData)
         {
+            // store the country from the map
             std::string country = data.first;
+
+            // write the country tag
             xmlFile << "\t<country>\n";
 
+            // write the country name, region, and code
             xmlFile << "\t\t<name>" << mData[country][0] << "</name>\n";
             xmlFile << "\t\t<region>" << mData[country][1] << "</region>\n";
             xmlFile << "\t\t<code>" << mData[country][2] << "</code>\n"; 
-            
-            // loop from 3 to end of this list
-            for (int i=3; i<mData[country].size (); ++i)
-            {
-                xmlFile << "\t\t\t<>" << mData[country][i] << "</>\n";
-            }
 
+            // write the cities opening tag
+            xmlFile << "\t\t<cities>\n";
+
+            // init a counter, start at 3 for the city information
+            int i = 3;
+            // loop through the rest of the country data
+            while (i <mData[country].size ())
+            {
+                // write out the city opening tag
+                xmlFile << "\t\t\t<city>\n";
+                // write out the city name, district, and population (using string to int for commas)
+                xmlFile << "\t\t\t\t<name>" << mData[country][i++] << "</name>\n";
+                xmlFile << "\t\t\t\t<district>" << mData[country][i++] << "</district>\n";
+                xmlFile << "\t\t\t\t<population>" << std::stoi(mData[country][i++]) << "</population>\n";
+
+                // write out the city closing tag
+                xmlFile << "\t\t\t</city>\n";
+            }
+            // close cities tag
+            xmlFile << "\t\t</cities>\n"; 
+            // close the country tag
             xmlFile << "\t</country>\n";
         }   
-
+        // close the countries tag
         xmlFile << "</countries>\n";
-
     }
+    // close the file
+    xmlFile.close ();
 }
 
+/*
+    This method checks if the string already exists in the country map
+    returns true if it does, false otherwise
+*/
 bool ConvertXML::CountryExists (const std::string &data)
 {
+    // ternary operator: true if we find the country, false otherwise
     return mData.find (data) == mData.end() ? false : true;
 }
 
+/*
+    This method extracts the data from the raw data vector
+    and adds the country data to a map
+*/
 void ConvertXML::ExtractData ()
 {
+    // for each data in the raw data vector
     for (auto data : mRawData)
     {
+        // init a string stream with the data
         std::stringstream ss (data);
+        // declare a string to hold the token
         std::string token;
+        // declare a vector that will hold the data
         std::vector<std::string> line;
+        // while we can split the data from the stringstream on the separator
         while (std::getline (ss, token, separator))
+            // add the token to the vector
             line.push_back (token);
 
         // data is for each line:
@@ -250,12 +332,18 @@ void ConvertXML::ExtractData ()
             4 : District
             5 : Population
         */
+        // store the current country
         std::string currentCountry = line[0];
         
+        // check if we already have this country
         if (!CountryExists (currentCountry))
+            // if not, then add the vector to the country map
             mData[currentCountry] = line;
+        // else we already have this country, so just add the cities
         else
+            // loop from 3 (city name) to 5 (population)
             for (int i=3; i<=5; ++i)
+                // add that data to the currenty country's vector
                 mData[currentCountry].push_back (line[i]);
     }
 }
